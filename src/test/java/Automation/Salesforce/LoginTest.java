d68e124bd4172d1e5956d2e4d1bbfb472d7bd7ec
rohit <rohitkumar995@gmail.com>
2021-09-29 17:05:52 +0530

    this is asian time zone dev

--- src/test/java/Automation/Salesforce/LoginTest.java
+++ src/test/java/Automation/Salesforce/LoginTest.java
@@ -26,6 +26,7 @@ public class LoginTest extends BaseSalesforces{
 		hp.getLogin().click();	
 		System.out.println("Logged in successfully??");
 		System.out.println("Not there is a credential error, credential mismatch");
+		System.out.println("Ok, I have resolved. Please check...!");
 	}
 	
 	@Test(priority=2)
